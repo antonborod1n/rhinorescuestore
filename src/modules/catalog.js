4a2ff6c04054a14f2aba5026b09788c6a73284f4
antonborod1n <antonborodin86@gmail.com>
2023-02-02 13:28:41 +0300

    fix drop menu

--- src/modules/catalog.js
+++ src/modules/catalog.js
@@ -2,14 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let catalogFilterBtn = document.querySelector('.catalog__filter-btn');
     let catalogFilterDropList = document.querySelector('.catalog__filter-drop-list');
 
-    catalogFilterBtn.addEventListener('click', function () {
-        catalogFilterDropList.classList.toggle('catalog__filter-drop-list--active');
+    document.addEventListener('click', function (e) {
+        if (e.target == catalogFilterBtn) {
+            catalogFilterDropList.classList.toggle('catalog__filter-drop-list--active');
+        } else {
+            console.log(e.target)
+            catalogFilterDropList.classList.remove('catalog__filter-drop-list--active')
+        }
     });
-
-
 });
-
-
-
-
-
